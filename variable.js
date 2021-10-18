612b6084821e4fafc35964441f92c5508ddab7ed
Drashti-711 <76907602+Drashti-711@users.noreply.github.com>
2021-10-18 21:23:35 +0530

    Prac 2
    Variable

---
+++ variable.js
@@ -0,0 +1,15 @@
+console.log(2+2)    //without using variable
+
+num = 2 + 2
+console.log(num )    //using variable
+
+console.log(num + 3)    //using variable
+
+a = 2
+b = 3
+console.log("direct print answer adding variable")
+console.log(a + b)
+
+c = a + b
+console.log("adding variable store result than print ")
+console.log(c)
