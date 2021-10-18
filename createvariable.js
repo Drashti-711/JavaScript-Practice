a970a031a3b5a4c59b9ee4818bd10e3f39622a4f
Drashti-711 <76907602+Drashti-711@users.noreply.github.com>
2021-10-18 21:33:26 +0530

    Prac 3
    Create Variable

---
+++ createvariable.js
@@ -0,0 +1,13 @@
+//declare variable using "let"
+ 
+let num = 7     //declare variable using "let"
+console.log(num)
+
+num = 12        //using declared variable another time
+console.log(num)
+
+let username = 'Drashti'   //to assign a string use "Double quotes " or "Single Quotes"
+console.log(username)
+
+let user = "Drashti's book" //to avoid confusion of "apostrophe" s(single quote) make use of double quote 
+console.log(user)
