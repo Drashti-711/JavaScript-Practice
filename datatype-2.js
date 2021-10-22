d73a718d1f79ed06e230a5852f8a74dc7967640c
Drashti-711 <76907602+Drashti-711@users.noreply.github.com>
2021-10-22 15:12:04 +0530

    Prac 6
    Datatypes Part-2

---
+++ datatype-2.js
@@ -0,0 +1,34 @@
+let FirstName = "Drashti"
+let LastName = "Kansare"
+
+//lets concate 2 string using "+"
+let name = FirstName + LastName
+let user = FirstName + " " + LastName
+/*
+Other method
+let user
+user = FirstName + " " + LastName
+*/
+console.log(name)
+console.log(user);
+
+/*  Use of "escape" Character
+It is used when we want to print the inside double quote portion of the statement written in double quote 
+as it is
+i.e to avoid the error of using double quote inside double quote
+*/
+let users = "Drashti Kansare \"Practice\" "
+console.log(users)
+
+/*
+\t for tab 
+\n tp print in new line
+*/
+
+let username = "Drash\tti \nKansare"
+console.log(username)
+
+//Boolean datatype
+let num = 5<7
+console.log(typeof num)
+console.log(num)
