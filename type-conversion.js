747ea135adfb5100be9e411306d25f82642bcf16
Drashti-711 <76907602+Drashti-711@users.noreply.github.com>
2021-10-22 22:26:52 +0530

    Prac 7
    Type Conversion

---
+++ type-conversion.js
@@ -0,0 +1,9 @@
+//Explicit Concersion
+let num = String(8)     //6 is the assigned value of num
+console.log(num, typeof num)
+
+num = "1234"  //num is assigned the value in form string
+console.log(num, typeof num)
+
+num = Number("123")   //here the value assigned is in form of string but is converted in number using "Number"
+console.log(num, typeof num)
