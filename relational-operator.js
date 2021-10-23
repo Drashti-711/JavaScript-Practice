1645d5aeef7ff23bcb040f0533f6af28267e8db7
Drashti-711 <76907602+Drashti-711@users.noreply.github.com>
2021-10-23 17:28:56 +0530

    Prac 10
    use of Relational operator

---
+++ relational-operator.js
@@ -0,0 +1,20 @@
+let x = 6 <= 6
+console.log(x)
+
+x = 7 
+y = 7
+
+let data = x <= y
+console.log(data)
+
+x = "pen"
+y = "pencil"
+
+data = x < y
+console.log(data)
+
+x = "3"
+y = 3
+data = x === y//to check quality compulsory use triple equal to as it ccompare not only value but also datatype
+console.log(data)
+
