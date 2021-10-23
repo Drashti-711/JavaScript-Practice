32f8d5091bed762ae7916600e28c294b6a25af6c
Drashti-711 <76907602+Drashti-711@users.noreply.github.com>
2021-10-23 17:28:15 +0530

    Prac 9
    use of Arithmetic operator

---
+++ arithmetic-operator.js
@@ -0,0 +1,30 @@
+let num1 = 4;
+let num2 = 2
+let result
+
+result = num1 + num2
+console.log("Addition : " , result)
+
+result = num1 * num2
+console.log("Multiply : " , result)
+
+result = num1 - num2
+console.log("Subtraction : " , result)
+
+result = num1 / num2
+console.log("Division : " , result)
+
+result = num1 % num2
+console.log("Remainder : " , result)
+
+let num = 4
+x = num++   //post increment
+console.log(x , num)
+
+num = 4
+x = ++num   //pre increment
+console.log(x , num)
+
+a = 4
+result = Math.pow(4,3)  //use of power funntion(value,power )
+console.log(result)
