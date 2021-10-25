d5b529f458a1a0db755d54fc789ed04ebb5e0de6
Drashti-711 <76907602+Drashti-711@users.noreply.github.com>
2021-10-25 13:37:10 +0530

    Prac 12
    if else Statement

---
+++ conditional-statement.js
@@ -0,0 +1,11 @@
+let num1 = 6 , num2 = 4;
+
+if (num1 > num2)
+{
+    console.log("num1 is greater than num2");
+    console.log(num1, "is greater than ", num2);       
+} 
+else 
+{
+    console.log("num2 is greater than num1");    
+}
