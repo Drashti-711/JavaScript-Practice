f47fa330b3b2712ef5db25876c5dc1ab6931de27
Drashti-711 <76907602+Drashti-711@users.noreply.github.com>
2021-10-25 13:37:58 +0530

    Prac 13
    if elseif Statement

---
+++ eg-conditional-statement.js
@@ -0,0 +1,14 @@
+let num1 = 12 , num2 = 11, num3 = 43;
+
+if (num1 > num2 && num1 > num3)
+{
+    console.log("num1 is greater");    
+} 
+else if(num2 > num1 && num2 > num3)
+{
+    console.log("num2 is greater");
+}
+else
+{
+    console.log("num3 is greater");
+}
