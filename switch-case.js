35535c7b252a5374975ca44080a2d6e2a7e449d8
Drashti-711 <76907602+Drashti-711@users.noreply.github.com>
2021-10-26 14:09:03 +0530

    Prac 15
    switch case

---
+++ switch-case.js
@@ -0,0 +1,32 @@
+let day = "Monday";
+
+switch (day) 
+{
+    case "Monday":
+        console.log(" Its Monday");
+        break;
+
+    case "Tuesday":
+        console.log(" Its Tuesday");
+        break;
+
+    case "Wednesday":
+        console.log(" Its Wednesday");
+        break;
+
+    case "Thursday":
+        console.log(" Its Thursday");
+        break;
+
+    case "Friday":
+        console.log(" Its Friday");
+        break;
+
+    case "Saturday":
+        console.log(" Its Saturday");
+        break;
+
+    default:
+        console.log("Nothing");
+        break;
+}
