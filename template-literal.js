eb0e4c4bfe948cc2952f66089e540e42122713a9
Drashti-711 <76907602+Drashti-711@users.noreply.github.com>
2021-10-26 14:09:27 +0530

    Prac 16

---
+++ template-literal.js
@@ -0,0 +1,6 @@
+let num1 = 7 , num2 = 12;
+let result = num1 + num2
+
+console.log(`The addition of ${num1} and ${num2} is ${result}`);
+
+console.log("The addition of " + num1 + "and " + num2  + " is " + result)
